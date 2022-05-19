35a8b2c167c3420d86784252b2394aee14b797ba
Baddala Sai Kumar Reddy <saibaddala172@gmail.com>
2022-05-19 18:32:13 +0530

    completed

--- lib/constants/urls.dart
+++ lib/constants/urls.dart
@@ -1,12 +1,12 @@
 class Urls {
   static String tracksUrl =
-      "https://api.musixmatch.com/ws/1.1/chart.tracks.get?apikey=2d782bc7a52a41ba2fc1ef05b9cf40d7";
+      "https://api.musixmatch.com/ws/1.1/chart.tracks.get?apikey=a3d497205432812d5a3b0ceb1549fe47";
 
   static String getTrackUrl(int trackId) {
-    return "https://api.musixmatch.com/ws/1.1/track.get?track_id=$trackId&apikey=2d782bc7a52a41ba2fc1ef05b9cf40d7";
+    return "https://api.musixmatch.com/ws/1.1/track.get?track_id=$trackId&apikey=a3d497205432812d5a3b0ceb1549fe47";
   }
 
   static String getTrackLyricsUrl(int trackId) {
-    return "https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=$trackId&apikey=2d782bc7a52a41ba2fc1ef05b9cf40d7";
+    return "https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=$trackId&apikey=a3d497205432812d5a3b0ceb1549fe47";
   }
 }
